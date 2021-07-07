317d3254e7e844172e27a71998a8a94805b2ed99
yuyihsiang <yi-hsiang.yu@nrel.gov>
2021-07-07 13:27:41 -0600

    Update MoorDyn to V1.01.02

--- MoorDyn.h
+++ MoorDyn.h
@@ -18,9 +18,17 @@
 #define __MOORDYN_H__
 
 #ifdef MoorDyn_EXPORTS     // this is set as a preprocessor definition!!!
-#define DECLDIR __declspec(dllexport)
+	#ifndef LINUX
+        #ifndef OSX
+            #define DECLDIR __declspec(dllexport)
+        #else
+            #define DECLDIR
+        #endif
+	#else
+		#define DECLDIR 
+	#endif
 #else
-#define DECLDIR //__declspec(dllimport)
+	#define DECLDIR //__declspec(dllimport)
 #endif
 
 #ifdef __cplusplus
@@ -28,9 +36,19 @@ extern "C"
 {
 #endif
 
+/*#ifndef LINUX
+#ifndef OSX
+ #include <Windows.h>
+#endif
+#endif*/
+
 int DECLDIR LinesInit(double X[], double XD[]);
 
 int DECLDIR LinesCalc(double X[], double XD[], double Flines[], double* , double* );
+
+int DECLDIR FairleadsCalc2(double rFairIn[], double rdFairIn[], double fFairIn[], double* t_in, double *dt_in); // easier to call version
+int DECLDIR FairleadsCalc(double **rFairIn, double **rdFairIn, double ** fFairIn, double* t_in, double *dt_in);
+
 
 int DECLDIR LinesClose(void);
 
@@ -38,10 +56,13 @@ double DECLDIR GetFairTen(int);
 
 int DECLDIR GetFASTtens(int* numLines, float FairHTen[], float FairVTen[], float AnchHTen[], float AnchVTen[] );
 
+int DECLDIR GetConnectPos(int l, double pos[3]);
+int DECLDIR GetConnectForce(int l, double force[3]);
+int DECLDIR GetNodePos(int LineNum, int NodeNum, double pos[3]);
+
 int DECLDIR DrawWithGL(void);
 
-void AllOutput(double);
-int SetupWavesFromFile(void);
+void AllOutput(double, double);
 
 #ifdef __cplusplus
 }
