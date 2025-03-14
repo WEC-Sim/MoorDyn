9159c4a47e1845aff8967317a164590bf5416624
jtgrasb <87095491+jtgrasb@users.noreply.github.com>
2025-03-14 10:04:27 -0600

    Allow for more dofs (more bodies) in MD call

--- MoorDyn_caller.m
+++ MoorDyn_caller.m
@@ -1,8 +1,8 @@
 % function to return any items of interest to coupling from MoorDyn at
 % every time step
 function FLines = MoorDyn_caller(X,XD,Time,CoupTime)
-    FLines_value = zeros(1,6);
-    FLines = zeros(1,6);
+    FLines_value = zeros(1,length(X));
+    FLines = zeros(1,length(X));
     FLines_p = libpointer('doublePtr',FLines_value);
     calllib('libmoordyn', 'MoorDynStep', X, XD, FLines_p, Time, CoupTime);
     FLines = FLines_p.value;
